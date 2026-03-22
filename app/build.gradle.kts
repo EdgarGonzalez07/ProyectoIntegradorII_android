4c112705b47ac729b28a4128a5f849dae6831052
Dulce127 <20243ds105@utez.edu.mx>
2026-03-21 20:42:22 -0600

    Vainas necesarias para que jale todo. Dependencias y esas cosas xd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,19 @@ android {
 }
 
 dependencies {
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // Kotlin Extensions (coroutines support)
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
@@ -51,10 +64,10 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.compose.material:material-icons-extended")
 
-    implementation(libs.androidx.room)
-    implementation(libs.androidx.room.ktx)
+    //implementation(libs.androidx.room)
+    //implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.compose.ui.text)
-    ksp(libs.androidx.room.compiler)
+    //ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.navigation)
     implementation(libs.retrofit)
     implementation(libs.gson)
